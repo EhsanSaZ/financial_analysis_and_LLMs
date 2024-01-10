5091884d664dd14e339a79466fadff758a2e7625
EhsanSaeedizade <e24saeedi@gmail.com>
2024-01-09 20:57:12 -0800

    Read confing data from os environment

--- Config.py
+++ Config.py
@@ -1,11 +1,13 @@
+import os
 import logging
 
-read_local_mock_articles = True
-use_mul_process = True
-num_processes = 5
-mock_data_file_path = "mock_data/mock_data.json"
-articles_dir = "articles"
-num_pages = 3
-log_level = logging.INFO
+read_local_mock_articles = os.getenv("READ_LOCAL_MOCK_ARTICLES", "True") == "True"
+use_mul_process = os.getenv("USE_MUL_PROCESS", "True") == "True"
+num_processes = int(os.getenv("NUM_PROCESSES", "5"))
+mock_data_file_path = os.getenv("MOCK_DATA_FILE_PATH", "mock_data/mock_data.json")
+articles_dir = os.getenv("ARTICLES_DIR", "articles")
+num_pages = int(os.getenv("NUM_PAGES", "3"))
+log_level = int(os.getenv("LOG_LEVEL", logging.INFO))
 
-OPENAI_API_KEY = "the KEY"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
+
