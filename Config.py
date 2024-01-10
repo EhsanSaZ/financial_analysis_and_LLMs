371a53a77bb7d26fa0619f6d9cd539e158724556
EhsanSaeedizade <e24saeedi@gmail.com>
2024-01-10 00:59:37 -0800

    Update Config

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 import logging
 
-read_local_mock_articles = os.getenv("READ_LOCAL_MOCK_ARTICLES", "True") == "True"
-use_mul_process = os.getenv("USE_MUL_PROCESS", "True") == "True"
+read_local_mock_articles = os.getenv("READ_LOCAL_MOCK_ARTICLES", None)
+use_mul_process = os.getenv("USE_MUL_PROCESS", None)
 num_processes = int(os.getenv("NUM_PROCESSES", "5"))
 mock_data_file_path = os.getenv("MOCK_DATA_FILE_PATH", "mock_data/mock_data.json")
 articles_dir = os.getenv("ARTICLES_DIR", "articles")
